b647a7584b023b08a3afb19a094e2e3e79c48735
Simon Oxford <simonoxford99@gmail.com>
2024-05-06 14:46:56 +0100

    Adding database + seeds ready for seeding

---
+++ database/seeds.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose'
+import 'dotenv/config'
+
+import User from '../models/User.js'
+import userData from './data/users.js'
+
+async function seedData(){
+
+  try {
+
+    await mongoose.connect(process.env.CONNECTION_STRING)
+    console.log('Database connection established.')
+
+    const deletedUsers = await User.deleteMany()
+    console.log(`${deletedUsers.deletedCount} users deleted.`)
+
+    const createdUsers = await User.create(userData)
+    console.log(`${createdUsers.length} users created.`)
+
+    const deletedCars = await Car.deleteMany()
+    console.log(`${deletedCars.deletedCount} cars deleted.`)
+
+    const carsWithOwners = carData.map(car => {
+      const userId = createdUsers[Math.floor(Math.random() * createdUsers.length)]._id
+      return { ...car, owner: userId }
+    })
+
+    const createdCars = await Car.create(carsWithOwners)
+    console.log(`${createdCars.length} cars added. `)
+
+    await mongoose.connection.close()
+    console.log('Connection close.')
+  } catch (error) {
+    console.log(error)
+
+    await mongoose.connection.close()
+    console.log('Connection closed')
+  }
+}
+seedData()
