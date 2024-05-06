517f0224edc0b9320375820732546d14c89e85f4
Simon Oxford <simonoxford99@gmail.com>
2024-05-06 16:12:42 +0100

    test and commit

--- models/Post.js
+++ models/Post.js
@@ -7,11 +7,11 @@ const postSchema = new mongoose.Schema(
         image: { type: String, required: true },
         comments: [commentSchema],
         likes: { type: [mongoose.ObjectId], ref: 'User' },
-        owner: { type: mongoose.ObjectId, ref: 'User' }
+        owner: { type: mongoose.ObjectId, ref: 'User' },
 
     },
     {
-        timestamps: true
+        timestamps: true,
     }
 )
 
