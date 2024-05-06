cdcc2fbf10218243569bbf8b9c31c88f8b767dad
Vivian <vivlopes95@gmail.com>
2024-05-06 20:19:40 +0100

    Added endpoints in router.js/ added controllers / created commo n.js with simple unauthorized handler and created secureRoute.js

---
+++ controllers/posts.js
@@ -0,0 +1,52 @@
+import  Post  from '../models/Post.js'
+import {Error} from 'mongoose'
+//! imort error handlers here
+
+//Get All Posts
+export const getPosts = async(req,res) => {
+    try {
+        console.log('Hit Get All Post Endpoint')
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+//Create Post
+export const createPost = async(req,res) => {
+    try {
+        console.log('Hit Create Post Endpoint')
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+//Single Post
+export const getPost = async(req,res) => {
+    try {
+        console.log('Hit View Single Post Endpoint')
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+
+//Update Post
+export const updatePost = async(req,res) => {
+    try {
+        console.log('Hit Update Post Endpoint')
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+//Delete Post
+export const deletePost = async(req,res) => {
+    try {
+        console.log('Hit Delete Post Endpoint')
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
--- models/Post.js
+++ models/Post.js
@@ -1,11 +1,14 @@
 import mongoose from "mongoose";
+//imported commentSchema
+import commentSchema from "./Comment.js";
 
 const postSchema = new mongoose.Schema(
     {
         title: { type: String, required: true },
         content: { type: String, required: true },
         image: { type: String, required: true },
-        comments: [commentSchema],
+        //Made correction here to fix endpoints. Slight syntax error
+        comments: [{type: commentSchema}],
         likes: { type: [mongoose.ObjectId], ref: 'User' },
         owner: { type: mongoose.ObjectId, ref: 'User' }
 
