be2a05f460243e4eaf39562a017e53b2243ae2f6
Simon Oxford <simonoxford99@gmail.com>
2024-05-07 09:29:37 +0100

    addign changes to database

---
+++ controllers/comments.js
@@ -0,0 +1,49 @@
+import Comment from '../models/Comment.js'
+import {Error} from 'mongoose'
+
+//GET ALL COMMENTS
+export const getComments = async(req,res) => {
+    try {
+        console.log('Hit ALL COMMENTS endpoint')
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+//CREATE NEW COMMENT
+export const createComment = async(req,res) => {
+    try {
+        console.log('Hit CREATE COMMENT endpoint')
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+//GET SINGLE COMMENT
+export const getComment = async(req,res) => {
+    try {
+        console.log('Hit SINBGLE COMMENT endpoint')
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+//UPDATE COMMENT
+export const updateComment = async(req,res) => {
+    try {
+        console.log('Hit UPDATE COMMENT endpoint')
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+//DELETE COMMENT
+export const deleteComment = async(req,res) => {
+    try {
+        console.log('Hit DELETE COMMENT endpoint')
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
---
+++ database/data/users2.js
@@ -0,0 +1,14 @@
+export default [
+    {
+        username: "admin",
+        email: "admin@email.com",
+        password: "pass",
+        passwordConfirmation: "pass"
+    },
+    {
+        username: "v",
+        email: "v@email.com",
+        password: "pass",
+        passwordConfirmation: "pass"
+    }
+]
