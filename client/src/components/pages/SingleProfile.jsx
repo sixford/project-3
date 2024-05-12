3b07791da5955446e8129ede275220386485134f
Kane Lawless <kanelawless@gmail.com>
2024-05-12 14:46:09 +0100

    fetching data for single profile success

--- client/src/components/pages/SingleProfile.jsx
+++ client/src/components/pages/SingleProfile.jsx
@@ -1,3 +1,26 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getToken } from "../../lib/auth";
+
 export default function SingleProfile() {
-    
+
+    const [userData, setUserData] = useState()
+    const params = useParams()
+
+    const args = { headers: { authorization: getToken() } }
+
+    useEffect(() => {
+        async function getData() {
+            try {
+                const { data } = await axios.get(`/api/profile/${params.userId}`, args)
+                setUserData(data)
+                console.log(data)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+
+        getData()
+    }, [])
 }
