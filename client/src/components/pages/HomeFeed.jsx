be9eb4aad28462010d2cf212ecd9aabb47bfd059
Kane Lawless <kanelawless@gmail.com>
2024-05-14 14:27:12 +0100

    homepage changes

--- client/src/components/pages/HomeFeed.jsx
+++ client/src/components/pages/HomeFeed.jsx
@@ -7,59 +7,58 @@ import backgroundImage from "../assets/future-showroom.jpg"
 
 export default function HomeFeed() {
 
-  // auth headers
-  const args = { headers: { authorization: getToken() } }
+    // auth headers
+    const args = { headers: { authorization: getToken() } }
 
-  const [postData, setPostData] = useState()
+    const [postData, setPostData] = useState()
 
-  const navigate = useNavigate()
+    const navigate = useNavigate()
 
-  function handleClick(e) {
-    // for now you must click the image to load the single post page
-    const _id = e.target.id
-    navigate(`/posts/${_id}`)
+    function handleClick(e) {
+        // for now you must click the image to load the single post page
+        const _id = e.target.id
+        navigate(`/posts/${_id}`)
 
-  }
-  // call homefeed endpoint for post data
-  useEffect(() => {
-    async function getPostData() {
-      try {
-        const data = await axios.get('/api/homefeed', args)
-        setPostData(data.data)
-        console.log(data.data)
-      } catch (error) {
-        console.log(error)
-      }
     }
-    getPostData()
-  }, [])
+    // call homefeed endpoint for post data
+    useEffect(() => {
+        async function getPostData() {
+            try {
+                const data = await axios.get('/api/homefeed', args)
+                setPostData(data.data)
+                console.log(data.data)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        getPostData()
+    }, [])
 
-  return (
-    <div
-      className="home-feed-container"
-      style={{ backgroundImage: `url(${backgroundImage})` }} // Set background image
-    >
-      <Container>
-        <h1 className="text-center">Home Feed - Recent posts from others you follow</h1>
-        <Row xs={1} sm={2} md={3} lg={4} xl={5} className="g-4">
-          {postData && postData.map(post => {
-            // destructure vital data
-            const { image, title, content, _id } = post
-            return (
-              // Generate card for each post
-              <Col key={_id}>
-                <Card style={{ cursor: "pointer" }} >
-                  <Card.Img src={image} alt={title} id={_id} onClick={handleClick} />
-                  <Card.Body>
-                    <Card.Title>{title}</Card.Title>
-                    <Card.Text>{content}</Card.Text>
-                  </Card.Body>
-                </Card>
-              </Col>
-            )
-          })}
-        </Row>
-      </Container>
-    </div>
-  )
+    return (
+        <div
+
+            style={{ backgroundImage: `url(${backgroundImage})` }} // Set background image
+        >
+            <Container className="home-feed-container">
+                <h1 className="text-center" style={{ color: 'white' }}>Home Feed - Recent posts from others you follow</h1>
+                <Row xs={1} sm={2} md={3} lg={4} xl={4} className="g-4">
+                    {postData && postData.map(post => {
+                        // destructure vital data
+                        const { image, title, content, _id } = post
+                        return (
+                            // Generate card for each post
+                            <Col key={_id}>
+                                <Card style={{ cursor: "pointer" }} >
+                                    <Card.Img src={image} alt={title} id={_id} onClick={handleClick} />
+                                    <Card.Body>
+                                        <Card.Title>{title}</Card.Title>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                        )
+                    })}
+                </Row>
+            </Container>
+        </div>
+    )
 }
