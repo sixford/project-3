2bd69e8042ebe3263342f5afe279b9f2fae7953f
Simon Oxford <simonoxford99@gmail.com>
2024-05-14 18:20:21 +0100

    added github links to footer, styled background for auth

--- client/src/components/pages/Footer.jsx
+++ client/src/components/pages/Footer.jsx
@@ -1,7 +1,35 @@
-export default function Footer(){
+import { Link } from 'react-router-dom'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+
+export default function Footer() {
   return (
-    <footer className="footer text-center bg-black py-3">
-      <span className="text-white">&copy; Pedal 2024</span>
+    <footer className='footer' style={{ background: 'linear-gradient(to right, white, black)' }}>
+      <Container>
+        <Row className='footer-row'>
+          <Col className='footer-col'>
+            <Link target='_blank' to='https://github.com/sixford' className='footer-link teal-text'>
+              <FontAwesomeIcon icon={faGithub} /> Simon Oxford
+            </Link>
+          </Col>
+          <Col className='footer-col'>
+            <Link target='_blank' to='https://github.com/KaneLawless' className='footer-link teal-text'>
+              <FontAwesomeIcon icon={faGithub} /> Kane Lawless
+            </Link>
+          </Col>
+          <Col className='footer-col'>
+            <Link target='_blank' to='https://github.com/8055-NVW' className='footer-link teal-text'>
+              <FontAwesomeIcon icon={faGithub} /> Vivian Lopes
+            </Link>
+          </Col>
+        </Row>
+      </Container>
     </footer>
   )
 }
