a89266d3a85115e7b21e184c303eaa263c76ed4b
Vivian <vivlopes95@gmail.com>
2024-05-15 10:53:29 +0100

    small changes made. added assets

--- database/data/users.js
+++ database/data/users.js
@@ -4,48 +4,48 @@ export default [
     email: 'admin@email.com',
     password: 'pass',
     passwordConfirmation: 'pass',
-    profilePic: 'https://via.placeholder.com/300'
+    profilePic: 'https://www.aquafeed.co.uk/wp-content/uploads/2023/04/male_profile-420x420.png'
   },
   {
     username: 'v',
     email: 'v@email.com',
     password: 'pass',
     passwordConfirmation: 'pass',
-    profilePic: 'https://via.placeholder.com/300'
+    profilePic: 'https://64.media.tumblr.com/494496dc03085887f6f967da7f3cf37c/6b7919f051dd2db3-10/s250x400/553375cc24381b670defc1d84e2e7ebf8cfcf28d.jpg'
   },
   {
     username: 'john_doe',
     email: 'john.doe@email.com',
     password: 'password123',
     passwordConfirmation: 'password123',
-    profilePic: 'https://via.placeholder.com/300'
+    profilePic: 'https://lh3.googleusercontent.com/proxy/YSDWfYaQwpIpwhR7N_HrQ2BxJh7yaJvE_1SPJkmS5rt5ZffubHKfFP6qQcE_VZpUnP1rcHz5ZLKyVcofRXrZ-q6BfVqbdZMwjKoGyfyTl5I3YwfiyjxNv6VSK731oKmOVoZer39U85UV-UU'
   },
   {
     username: 'jane_smith',
     email: 'jane.smith@email.com',
     password: 'password456',
     passwordConfirmation: 'password456',
-    profilePic: 'https://via.placeholder.com/300'
+    profilePic: 'https://pub-static.fotor.com/assets/projects/pages/d5bdd0513a0740a8a38752dbc32586d0/300w/fotor-03d1a91a0cec4542927f53c87e0599f6.jpg'
   },
   {
     username: 'mike_jackson',
     email: 'mike.jackson@email.com',
     password: 'password789',
     passwordConfirmation: 'password789',
-    profilePic: 'https://via.placeholder.com/300'
+    profilePic: 'https://static-cdn.jtvnw.net/jtv_user_pictures/c6d27ad3-a5df-4b9b-804d-227dfa216ddd-profile_image-300x300.png'
   },
   {
     username: 'sara_williams',
     email: 'sara.williams@email.com',
     password: 'passwordabc',
     passwordConfirmation: 'passwordabc',
-    profilePic: 'https://via.placeholder.com/300'
+    profilePic: 'https://pub-static.fotor.com/assets/projects/pages/1bd9e5e88594483ba733ca32245f007f/300w/fotor-39237a95a12545389fcfaf11a1e97c69.jpg'
   },
   {
     username: 'chris_evans',
     email: 'chris.evans@email.com',
     password: 'passworddef',
     passwordConfirmation: 'passworddef',
-    profilePic: 'https://via.placeholder.com/300'
+    profilePic: 'https://play-lh.googleusercontent.com/HnzbI7urJlB6V26dtKiawYoBrH4iR5DAAk4KqNZzIa0NRWQukskR6aX7IrV55AULKIgA'
   }
 ]
